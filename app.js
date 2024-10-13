6397fe7b64e6ad306e6a195cb6e96dfce9dc89ad
Monir Hossain <monirhdigital@gmail.com>
2024-10-13 20:04:56 +0600

    version 2.0

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+
 const express = require('express');
+const  cors = require('cors')
 const usersRoute = require('./routes/users');
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 app.use(usersRoute);
+app.use(cors());
+
+
 module.exports = app;
